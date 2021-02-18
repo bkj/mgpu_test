049d11fc662c6749e02ad80bb239edbff65f1da2
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2021-02-18 23:09:02 +0000

    download

---
+++ download.sh
@@ -0,0 +1,6 @@
+ wget https://graphchallenge.s3.amazonaws.com/synthetic/graph500-scale18-ef16/graph500-scale18-ef16_adj.mmio.gz # |V|=262,144 / |E|=4,194,304
+ wget https://graphchallenge.s3.amazonaws.com/synthetic/graph500-scale19-ef16/graph500-scale19-ef16_adj.mmio.gz
+ wget https://graphchallenge.s3.amazonaws.com/synthetic/graph500-scale20-ef16/graph500-scale20-ef16_adj.mmio.gz
+ wget https://graphchallenge.s3.amazonaws.com/synthetic/graph500-scale21-ef16/graph500-scale21-ef16_adj.mmio.gz
+ wget https://graphchallenge.s3.amazonaws.com/synthetic/graph500-scale22-ef16/graph500-scale22-ef16_adj.mmio.gz # |V|=4,194,304 / |E|=67,108,864
+ wget https://graphchallenge.s3.amazonaws.com/synthetic/graph500-scale24-ef16/graph500-scale24-ef16_adj.mmio.gz
