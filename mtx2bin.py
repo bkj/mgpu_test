91e07688c12b9419ffdd9cab73d5e7756fcafd76
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2021-02-18 23:06:33 +0000

    adding data scripts

---
+++ mtx2bin.py
@@ -0,0 +1,36 @@
+import sys
+import argparse
+import numpy as np
+from scipy.io import mmread
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inpath', type=str, default='graph500-scale18-ef16_adj.mmio')
+    args = parser.parse_args()
+    
+    args.outpath = args.inpath.replace('.mmio', '.bin').replace('.mtx', '.bin')
+    
+    return args
+
+args = parse_args()
+
+print(f"reading: {args.inpath}", file=sys.stderr)
+adj = mmread(args.inpath).tocsr()
+
+sel = np.random.permutation(adj.shape[0])
+adj = adj[sel][:,sel]
+
+shape   = np.array(adj.shape).astype(np.int32)
+nnz     = np.array([adj.nnz]).astype(np.int32)
+indptr  = adj.indptr.astype(np.int32)
+indices = adj.indices.astype(np.int32)
+data    = adj.data.astype(np.float32)
+
+print(f"writing: {args.outpath}", file=sys.stderr)
+with open(args.outpath, 'wb') as f:
+    _ = f.write(bytearray(shape))
+    _ = f.write(bytearray(nnz))
+    _ = f.write(bytearray(indptr))
+    _ = f.write(bytearray(indices))
+    _ = f.write(bytearray(data))
+    f.flush()
